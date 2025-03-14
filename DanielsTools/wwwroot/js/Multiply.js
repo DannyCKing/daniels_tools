16dedf584be278e48ca549a0470155843b697117
Daniel King <dking@deseretmgt.com>
2025-03-14 08:23:23 -0600

    Layout for touch screens.

--- DanielsTools/wwwroot/js/Multiply.js
+++ DanielsTools/wwwroot/js/Multiply.js
@@ -23,6 +23,12 @@ var timerEnd = Date.now();
 var correctCount = 0;
 var stopTimerStatus = false;
 
+window.mobileAndTabletCheck = function () {
+    let check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+
 function getCookieName(username, firstOrSecond, number) {
     var partOne = "";
     if (firstOrSecond == 1) {
@@ -471,19 +477,41 @@ function turnNumberOnOff(buttonId) {
 
     const items = buttonId.split("_");
 
-    //we set the id of the number button to numberbutton_1_5
-    // we the one represents is the first number of the two operations
-    // and the 5 represents the number we are operating against
-    var isFirstNumber = items[1] == 1;
-    var numberToSet = items[2];
-
-
-    if (isFirstNumber) {
-        currentUser.Settings.FirstNumber[numberToSet] = !currentUser.Settings.FirstNumber[numberToSet];
+    if (items.length < 2) {
+        //not valid
+        return;
+    }
+    else if (items.length == 2) {
+        //specific number portion not found, this is the all on button
+        var isFirst = items[1] == 1;
+        if (isFirst) {
+            for (var i = 0; i <= 12; i++) {
+                currentUser.Settings.FirstNumber[i] = true;
+            }
+        }
+        else {
+            for (var i = 0; i <= 12; i++) {
+                currentUser.Settings.SecondNumber[i] = true;
+            }
+        }
     }
     else {
-        currentUser.Settings.SecondNumber[numberToSet] = !currentUser.Settings.SecondNumber[numberToSet];
+        //we set the id of the number button to numberbutton_1_5
+        // we the one represents is the first number of the two operations
+        // and the 5 represents the number we are operating against
+        var isFirstNumber = items[1] == 1;
+        var numberToSet = items[2];
+
+
+        if (isFirstNumber) {
+            currentUser.Settings.FirstNumber[numberToSet] = !currentUser.Settings.FirstNumber[numberToSet];
+        }
+        else {
+            currentUser.Settings.SecondNumber[numberToSet] = !currentUser.Settings.SecondNumber[numberToSet];
+        }
     }
+
+
 
     saveCurrentUserSettingsIntoCookies();
 
@@ -640,9 +668,17 @@ function showNumbersForProblem() {
     currentAnswer = getAnswer(firstNumber, secondNumber, operation);
 
     $("#firstNumberDiv").html(firstNumber);
+    $("#firstNumberDivSmallScreen").html(firstNumber);
+
     $("#secondNumberDiv").html(secondNumber);
+    $("#secondNumberDivSmallScreen").html(secondNumber);
+
     $("#operationDiv").html(operation);
-    $("#answerTextBox").trigger('focus');
+    $("#operationDivSmallScreen").html(operation);
+
+    if (mobileAndTabletCheck() == false) {
+        $("#answerTextBox").trigger('focus');
+    }
 }
 
 function showProblems(isTimed) {
@@ -717,6 +753,8 @@ function tickTimer() {
     var interval = 300;
 
     $('#secondsSpan').html("00:" + pad2(wholeSeconds));
+    $('#secondsSpanSmallScreen').html("00:" + pad2(wholeSeconds));
+
 
     if (wholeSeconds < 0) {
         wholeSeconds = 0;
@@ -739,33 +777,74 @@ function stopTimer() {
 }
 
 function makeAnswerTextGreenForShortTime() {
-    //$("#answerTextBox").css("border", "5px solid green");//more efficient
     $("#answerTextBox").css("background-color", "green");
+    $("#answerTextBoxSmallScreenPotrait").css("background-color", "green");
 
-    //$('#answerTextBox').addClass('green_box');
-    //$('#answerTextBox').animate({ backgroundColor: '#008000' }, 500);
     setTimeout(function () {
         clearAnswerBox();
     }, 500)
 }
 
 function makeAnswerRed() {
-    //$("#answerTextBox").css("border", "5px solid green");//more efficient
     $("#answerTextBox").css("background-color", "red");
+    $("#answerTextBoxSmallScreenPotrait").css("background-color", "red");
 }
 
 function makeAnswerClear() {
-    //$("#answerTextBox").css("border", "5px solid green");//more efficient
     $("#answerTextBox").css("background-color", "white");
+    $("#answerTextBoxSmallScreenPotrait").css("background-color", "white");
+
 }
 
 function clearAnswerBox() {
     $("#answerTextBox").val("");
-    //$("#answerTextBox").css("border-color", "black");
+    $("#answerTextBoxSmallScreenPotrait").val("");
     $("#answerTextBox").css("background-color", "white");
-    //$('#answerTextBox').removeClass('green_box');
+    $("#answerTextBoxSmallScreenPotrait").css("background-color", "white");
 }
 
+function checkMyAnswer(myAnswer) {
+
+    // we only allow numbers or space to by typed
+    myAnswer = myAnswer.replace(/[^\d ]+/g, '');
+
+    if (myAnswer.includes(" ")) {
+        // clear out on a space bar
+        myAnswer = "";
+    }
+
+    // update text box with properly formatted answer
+
+    $("#answerTextBox").val(myAnswer);
+    $("#answerTextBoxSmallScreenPotrait").val(myAnswer);
+
+    if (myAnswer == currentAnswer.toString() &&
+        myAnswer.length == currentAnswer.toString().length) {
+        //makeTextGreenForShortTime();
+        correctCount++;
+        updateCorrectCount();
+        beep();
+        showNumbersForProblem();
+        makeAnswerTextGreenForShortTime();
+
+        if (mobileAndTabletCheck() == false) {
+            $("#answerTextBox").trigger('focus');
+        }
+    }
+    else if (myAnswer.length < currentAnswer.toString().length) {
+        // my answer length is less than actual answer length
+        makeAnswerClear();
+    }
+    else if (myAnswer.length == currentAnswer.toString().length) {
+        makeAnswerRed();
+    }
+    else if (myanswer.length > 3) {
+        // ignore any input longer than length
+        var newAnswer = myAnswer.substring(0, 3);
+        $("#answerTextBox").val(newAnswer);
+        $("#answerTextBoxSmallScreenPotrait").val(newAnswer);
+    }
+}
 
 // document.ready
 $(function () {
@@ -853,6 +932,8 @@ $(function () {
         var buttonId = $(this).attr('id');
         turnOperationOnOff(buttonId)
     });
+    
+
 
     // on emoji search
     $('#emojiSearchBox').on('input', function (e) {
@@ -892,10 +973,18 @@ $(function () {
     })
 
     $("#stop_timing_button").on('click', function (event) {
+        stopTiming();
+    })
+
+    $("#stop_timing_button_small").on('click', function (event) {
+        stopTiming();
+    })
+
+    function stopTiming() {
         hideProblems(true);
         hideTimer();
         stopTimer();
-    })
+    }
 
     $("#onChooseNameButton").on('click', function (event) {
         // update name field with the existing fields
@@ -907,48 +996,33 @@ $(function () {
         newUser.UserId = currentUser.UserId;
     })
 
-    $('#answerTextBox').on('input', function (e) {
-        var myanswer = $("#answerTextBox").val();
 
-        // we only allow numbers or space to by typed
-        myanswer = myanswer.replace(/[^\d ]+/g, '');
-
-        if (myanswer.includes(" ")) {
-            // clear out on a space bar
-            myanswer = "";
+    $(".current_problem_small").on('click', function (event) {
+        var buttonId = $(this).attr('id');
+        var buttonValue = buttonId.replace(/\D/g, '');
+        if (buttonValue == "") {
+            buttonValue = " "
         }
-
-        // update text box with properly formatted answer
-        
-        $("#answerTextBox").val(myanswer);
-
-        if (myanswer == currentAnswer.toString() &&
-            myanswer.length == currentAnswer.toString().length) {
-            //makeTextGreenForShortTime();
-            correctCount++;
-            updateCorrectCount();
-            beep();
-            showNumbersForProblem();
-            makeAnswerTextGreenForShortTime();
-            
-            $("#answerTextBox").trigger('focus');
-        }
-        else if (myanswer.length < currentAnswer.toString().length) {
-            // my answer length is less than actual answer length
-            makeAnswerClear();
-        }
-        else if (myanswer.length == currentAnswer.toString().length) {
-            makeAnswerRed();
-        }
-        else if (myanswer.length > 3) {
-            // ignore any input longer than length
-            var newAnswer = myanswer.substring(0, 3);
-            $("#answerTextBox").val(newAnswer);
-        }
-
-
-
+        var currentText = $("#answerTextBoxSmallScreenPotrait").val();
+        var newAnswer = currentText + buttonValue;
+        var currentText = $("#answerTextBoxSmallScreenPotrait").val(newAnswer);
+        checkMyAnswer(newAnswer);
     });
+
+    $('#answerTextBoxSmallScreenPotrait').on('input', function (e) {
+        var myAnswer = $("#answerTextBoxSmallScreenPotrait").val();
+        checkMyAnswer(myAnswer);
+    })
+
+    $('#answerTextBox').on('input', function (e) {
+        var myAnswer = $("#answerTextBox").val();
+
+        checkMyAnswer(myAnswer);
+
+
+
+
+    })
 
 
 });
