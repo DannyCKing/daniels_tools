3b6e65f02e924f8335f58fd9106897107d4b75f2
Daniel King <dking@deseretmgt.com>
2025-03-15 08:05:21 -0600

    Deleting users now working
    Changing name now working.
    
    Removing boring EMOJIS_STRING

--- DanielsTools/wwwroot/js/Multiply.js
+++ DanielsTools/wwwroot/js/Multiply.js
@@ -11,11 +11,14 @@ var localUsers = {};
 var currentUser = {};
 currentUser.UserId = 0;
 currentUser.Settings = {};
+currentUser.OriginalUsername = "";
+currentUser.Username = "";
 
 var currentAnswer = 0;
 
 var newUser = {};
 newUser.Username = "";
+newUser.OriginalUsername = "";
 newUser.AvatarId = 0;
 
 var timerEnd = Date.now();
@@ -28,20 +31,6 @@ window.mobileAndTabletCheck = function () {
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
     return check;
 };
-
-function getCookieName(username, firstOrSecond, number) {
-    var partOne = "";
-    if (firstOrSecond == 1) {
-        partOne = "First";
-    }
-    else {
-        partOne = "Second";
-    }
-
-    var partTwo = number;
-
-    return username + "_" + partOne + "_" + partTwo;
-}
 
 function getCookie(cname) {
     let name = cname + "=";
@@ -59,10 +48,24 @@ function getCookie(cname) {
     return "";
 }
 
+function deleteCookie(name) {
+    var domain = window.location.hostname;
+
+    if (getCookie(name)) {
+        document.cookie = name + "=;path=/;domain=" + domain + ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    }
+}
+
 function loadUserCookies() {
     localUsers = {};
     localUsers.users = [];
 
+    loadUserIntoMemoryFromCookies();
+
+    reloadUserListFromMemory();
+}
+
+function loadUserIntoMemoryFromCookies() {
     var userCookieString = getCookie(USER_LIST_COOKIE_NAME);
 
     if (userCookieString) {
@@ -75,8 +78,6 @@ function loadUserCookies() {
             localUsers.users = [];
         }
     }
-
-    reloadUserListFromMemory();
 }
 
 function reloadUserListFromMemory() {
@@ -108,10 +109,6 @@ function setCookie(cookieName, cookieValue) {
     document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
 }
 
-function getElementName(firstOrSecond, number) {
-
-}
-
 
 function isAlphaNumeric(str) {
     var code, i, len;
@@ -137,16 +134,29 @@ function setNewUserUsername() {
         return;
     }
 
-    if (!isAlphaNumeric(newusername)) {
-        errorString = "Name can only contains letters and numbers";
+    if (newusername.length > 9) {
+        errorString = "Name is too long";
         $("#nameErrorSpan").html(errorString);
         return;
     }
+
+    //if (!isAlphaNumeric(newusername)) {
+    //    errorString = "Name can only contains letters and numbers";
+    //    $("#nameErrorSpan").html(errorString);
+    //    return;
+    //}
 
     // clear name error if it exists
     $("#nameErrorSpan").html("");
 
     newUser.Username = newusername;
+
+    if (!newUser.OriginalUsername) {
+        // if the new user original user name is not set,
+        // it is a new user
+        // if it is set, this is an existing user
+        newUser.OriginalUsername = newusername;
+    }
 
     // clear out current emoji
     newUser.AvatarId = 0;
@@ -184,13 +194,19 @@ function getTimeStamp() {
 }
 
 function saveCurrentUserSettingsIntoCookies() {
-    var userCookieName = USERS_SETTINGS_COOKIE_NAME.replace(USER_REPLACEMENT, currentUser.UserId);
+    var userCookieName = getCurrentUserCookieName()
     var settingsString = JSON.stringify(currentUser.Settings);
     setCookie(userCookieName, settingsString);
 }
 
+function getCurrentUserCookieName() {
+    var friendlyUserName = currentUser.OriginalUsername.toLowerCase().replace(/\W/g, '');
+    var userCookieName = USERS_SETTINGS_COOKIE_NAME.replace(USER_REPLACEMENT, currentUser.UserId + "_" + friendlyUserName);
+    return userCookieName;
+}
+
 function loadCurrentUsersSettingsFromCookies() {
-    var userCookieName = USERS_SETTINGS_COOKIE_NAME.replace(USER_REPLACEMENT, currentUser.UserId);
+    var userCookieName = getCurrentUserCookieName()
     var userSettingsString = getCookie(userCookieName);
 
     if (userSettingsString == "") {
@@ -478,11 +494,11 @@ function turnNumberOnOff(buttonId) {
     const items = buttonId.split("_");
 
     if (items.length < 2) {
-        //not valid
+        //not valid button that was pressed
         return;
     }
     else if (items.length == 2) {
-        //specific number portion not found, this is the all on button
+        //specific number portion not found, this is the "All On" button
         var isFirst = items[1] == 1;
         if (isFirst) {
             for (var i = 0; i <= 12; i++) {
@@ -492,7 +508,7 @@ function turnNumberOnOff(buttonId) {
         else {
             for (var i = 0; i <= 12; i++) {
                 currentUser.Settings.SecondNumber[i] = true;
-            }
+            }W
         }
     }
     else {
@@ -502,7 +518,6 @@ function turnNumberOnOff(buttonId) {
         var isFirstNumber = items[1] == 1;
         var numberToSet = items[2];
 
-
         if (isFirstNumber) {
             currentUser.Settings.FirstNumber[numberToSet] = !currentUser.Settings.FirstNumber[numberToSet];
         }
@@ -511,11 +526,38 @@ function turnNumberOnOff(buttonId) {
         }
     }
 
-
+    //checkIfAtLeastOneNumberIsOn();
 
     saveCurrentUserSettingsIntoCookies();
 
     turnOnOffNumberButtonsBasedOnSettings();
+}
+
+function checkIfAtLeastOneNumberIsOn() {
+    var doesFirstOneHaveOneOn = false;
+    for (var i = 0; i <= 12; i++) {
+        if (currentUser.Settings.FirstNumber[i]) {
+            doesFirstOneHaveOneOn = true;
+            break;
+        }
+    }
+
+    if (doesFirstOneHaveOneOn == false) {
+        //none of the first numbers are on, turn on 1
+        currentUser.Settings.FirstNumber[1] = true;
+    }
+
+    var doesSecondOneHaveOneOn = false;
+    for (var i = 0; i <= 12; i++) {
+        if (currentUser.Settings.SecondNumber[i]) {
+            doesSecondOneHaveOneOn = true;
+        }
+    }
+
+    if (doesSecondOneHaveOneOn == false) {
+        //none of the first numbers are on, turn on 1
+        currentUser.Settings.SecondNumber[1] = true;
+    }
 }
 
 
@@ -846,6 +888,34 @@ function checkMyAnswer(myAnswer) {
     }
 }
 
+function deleteCurrentUser() {
+    loadUserIntoMemoryFromCookies();
+
+    var indexToRemove = -1;
+
+    for (var i = 0; i < localUsers.users.length; i++) {
+        if (currentUser.UserId == localUsers.users[i].UserId) {
+            indexToRemove = i;
+            break;
+        }
+    }
+
+    if (indexToRemove != -1) {
+        localUsers.users.splice(indexToRemove, 1);
+    }
+
+    saveUserListToCookie();
+
+    var cookieName = getCurrentUserCookieName();
+    deleteCookie(cookieName);
+}
+
+function stopTiming() {
+    hideProblems(true);
+    hideTimer();
+    stopTimer();
+}
+
 // document.ready
 $(function () {
 
@@ -976,15 +1046,20 @@ $(function () {
         stopTiming();
     })
 
+    $("#deleteUserButton").on('click', function (event) {
+        var warningMessage = "Are you sure you want to delete the user " + currentUser.Username + "?";
+        $("#deleteUserModalBodyText").html(warningMessage);
+        $('#deleteUserModal').modal('show');
+    })
+
+    $("#deleteConfirmationButton").on('click', function (event) {
+        deleteCurrentUser();
+        location.reload();
+    })
+
     $("#stop_timing_button_small").on('click', function (event) {
         stopTiming();
     })
-
-    function stopTiming() {
-        hideProblems(true);
-        hideTimer();
-        stopTimer();
-    }
 
     $("#onChooseNameButton").on('click', function (event) {
         // update name field with the existing fields
@@ -994,6 +1069,7 @@ $(function () {
 
         newUser = {};
         newUser.UserId = currentUser.UserId;
+        newUser.OriginalUsername = currentUser.OriginalUsername;
     })
 
 
@@ -1018,10 +1094,6 @@ $(function () {
         var myAnswer = $("#answerTextBox").val();
 
         checkMyAnswer(myAnswer);
-
-
-
-
     })
 
 
